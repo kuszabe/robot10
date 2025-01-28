bfd075e587174fc1c58a35f8601e80846c68d462
kuszabe <kuszabe@gmail.com>
2025-01-28 16:28:48 +0100

    új movement a bálnának

--- balna.py
+++ balna.py
@@ -3,11 +3,6 @@
 from time import sleep
 import time
 
-starttime = time.time()
-
-def run_time():
-    return time.time() - starttime
-
 import helper
 
 from ev3dev2.motor import MoveTank, LargeMotor, MediumMotor
@@ -17,17 +12,20 @@ m = MoveTank("D", "A")
 ml = LargeMotor("D")
 mr = LargeMotor("A")
 g = GyroSensor("in2")
-bal_feltet = MediumMotor("C")
 jobb_feltet = MediumMotor("B")
-
+bal_feltet = MediumMotor("C")
 
 
 g.reset()
 g.calibrate()
 
-#fancy move function
+starttime = time.time()
+
+def run_time():
+    return time.time() - starttime
+
 #implement easing
-def turn(degree, speed = 0.5, easein= 60, easeout = 120):
+def turn(degree, speed = 0.8, easein= 60, easeout = 120):
     MIN_MOVE = 5
     maxdistance = degree - g.angle
     while True:
@@ -50,13 +48,16 @@ def turn(degree, speed = 0.5, easein= 60, easeout = 120):
         print("elcseszte", g.angle)
     m.off()
 
-def move(dist, speed = 0.5, easein = 100, easeout = 200, startgyro = None):
+
+def move(dist, speed = 0.7, easein = 100, easeout = 200, startgyro = None, CORRECTION_NODIFIER = 1):
+    print("moving")
     MIN_MOVE = 2
     if startgyro == None:
         startgyro = g.angle
     startpos = (mr.position + ml.position) / 2
     endpos = startpos + dist
     maxdistance = endpos - startpos
+    #írd ki a startgyro értékét
     print(startgyro)
     while True:
         currentpos = (mr.position + ml.position) / 2
@@ -73,40 +74,43 @@ def move(dist, speed = 0.5, easein = 100, easeout = 200, startgyro = None):
         else:
             sign = distance / helper.abs(distance)
             move = helper.clamp(speed * 100 * sign, -100, 100)
-        gyrooffset = startgyro - g.angle
+        gyrooffset = (startgyro - g.angle) * CORRECTION_NODIFIER * abs(move / 100)
         m.on(helper.clamp(move+gyrooffset, -100, 100), helper.clamp(move-gyrooffset, -100, 100))
-        # m.on(move, move)
-        print(g.angle, startgyro)
+        #írd ki a jelenlegi szögértéket és a startgyro értékét és a gyrooffset értékét
+        print(g.angle, startgyro, gyrooffset)
     m.off()
 
 
 
 ##futás kód
-bal_feltet.off()
-jobb_feltet.off()
+try:
+    bal_feltet.off()
+    jobb_feltet.off()
 
-move(700)
-turn(-45)
-move(665)
-turn(45)
-move(680, startgyro = 45)
-#kiengedi a rákot
-bal_feltet.on_for_degrees(20, -100)
-sleep(1)
-bal_feltet.on_for_degrees(20, 100)
+    jobb_feltet.on_for_degrees(20, 180, block=False)
+    move(700)
+    turn(-45)
+    move(665)
+    turn(44, speed=0.2)
+    move(680, startgyro = 45)
+    #kiengedi a rákot
+    bal_feltet.on_for_degrees(20, -100)
+    sleep(1)
+    bal_feltet.on_for_degrees(20, 100)
 
-#visszatolat
+    #visszatolat
 
-move(-600)
-#párhuzamosan fordul a szonárra
-turn(-53)
-#odamegy a szonár mellé
-move(320)
-#
-jobb_feltet.on_for_degrees(20, 300)
+    move(-600)
+    #párhuzamosan fordul a szonárra
+    turn(-60)
+    #odamegy a szonár mellé
+    jobb_feltet.on_for_degrees(20, 360, block=False)
+    move(450)
+    #
+    jobb_feltet.on_for_degrees(20, 720)
+finally:
+    m.off(brake=False)
 
-m.off(brake=False)
+    bal_feltet.off(brake=False)
 
-bal_feltet.off(brake=False)
-
-print("done in", round(run_time(), 1))
+    print("done in", round(run_time(), 1))
