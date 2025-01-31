d3a4781744a786404fb9891168d1cca69e0193bd
kuszabe <kuszabe@gmail.com>
2025-01-31 16:45:57 +0100

    motorcsere megoldva

--- hajo.py
+++ hajo.py
@@ -19,10 +19,14 @@ jobb_feltet = MediumMotor("B")
 
 g.calibrate()
 
+starttime = time.time()
 
-#fancy move function
-def turn(degree, speed = 0.8, easein= 60, easeout = 120):
-    MIN_MOVE = 7
+def run_time():
+    return time.time() - starttime
+
+def turn(degree, speed = 0.3, easein= 30, easeout = 60):
+    print("turning")
+    MIN_MOVE = 2
     maxdistance = degree - g.angle
     while True:
         distance = degree - g.angle
@@ -42,9 +46,14 @@ def turn(degree, speed = 0.8, easein= 60, easeout = 120):
         # print(g.angle, distance, move)
     if degree != g.angle:
         print("elcseszte", g.angle)
+        if abs(degree - g.angle) > 5:
+            turn(degree)
     m.off()
+    print(g.angle)
 
-def move(dist, speed = 0.7, easein = 100, easeout = 200, startgyro = None, CORRECTION_NODIFIER = 1):
+
+def move(dist, speed = 0.7, easein = 70, easeout = 150, startgyro = None, CORRECTION_NODIFIER = 0.5):
+    print("moving")
     MIN_MOVE = 2
     if startgyro == None:
         startgyro = g.angle
@@ -68,10 +77,10 @@ def move(dist, speed = 0.7, easein = 100, easeout = 200, startgyro = None, CORRE
         else:
             sign = distance / helper.abs(distance)
             move = helper.clamp(speed * 100 * sign, -100, 100)
-        gyrooffset = (startgyro - g.angle) * CORRECTION_NODIFIER * move / 100
+        gyrooffset = (startgyro - g.angle) * CORRECTION_NODIFIER * abs(move / 100)
         m.on(helper.clamp(move+gyrooffset, -100, 100), helper.clamp(move-gyrooffset, -100, 100))
-        #írd ki a jelenlegi szögértéket és a startgyro értékét
-        print(g.angle, startgyro)
+        #írd ki a jelenlegi szögértéket és a startgyro értékét és a gyrooffset értékét
+        print(g.angle, startgyro, gyrooffset)
     m.off()
 
 def move_with_turn_offset(dist, speed = 0.8, easein = 100, easeout = 200, startgyro = None, CORRECTION_MODIFIER = 3, turn_offset = 0):
@@ -105,25 +114,22 @@ def move_with_turn_offset(dist, speed = 0.8, easein = 100, easeout = 200, startg
         print(g.angle, startgyro)
     m.off()
 
-starttime = time.time()
-
-def run_time():
-    return time.time() - starttime
 
 try:
     ##futás kód
-    move(2000, CORRECTION_NODIFIER=4)
-    move(-100)
-    turn(20)
-    move(300, startgyro=20)
+    move(1000)
+    move(700, CORRECTION_NODIFIER=3, startgyro=0)
+    move(-200)
+    turn(25)
+    move(300)
     turn(0)
     bal_feltet.on_for_degrees(10, 90)
-    bal_feltet.on_for_seconds(10, 1)
-    move(-300, startgyro=0, speed=0.15)
-    move(-400)
-    bal_feltet.on_to_position(100, 0, block=False)
+    bal_feltet.on_for_seconds(10, 1, block=False)
+    move(-200, startgyro=0, speed=0.08)
+    move(-350)
+    bal_feltet.on_for_degrees(10, -200)
     turn(20)
-    move(1200, CORRECTION_NODIFIER=0)
+    move(1000, CORRECTION_NODIFIER=0)
     move(-100)
     
 
